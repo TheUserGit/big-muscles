df30ed8afb571c7a9025cd0110edb42f6ec28d41
Valentin <theuservalentin@gmail.com>
2016-12-02 05:57:08 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,67 @@
+//function openNav() {
+//    document.getElementsByClassName("page-nav").style.width = "250px";
+//}
+//
+//function closeNav() {
+//    document.getElementsByClassName("page-nav").style.width = "0";
+//}
+
+//
+//$("article").scrollTop(function() {
+//	$("fieldset").css({ "display" : "none"});
+//});
+
+
+
+
+
+//$(".get-btn").on("click", function() {
+//	$(".mail-jpg").css({"display": "block"});
+//	$(".mail-jpg").animate({'width': '+=30px'});
+//});
+
+
+//$(document).ready(function() {
+//	$(".page-nav").on("click", "a", function(event) {
+//		event.preventDefault();
+//		
+//		var id = $(this).attr('href');
+//		
+//		var top = $(id).offset().top;
+//		$('body, html').animate({scrollTop: top}, 300);
+//	});
+//	
+//	
+//	
+//	$(".arrow").on("click", "a", function(event) {
+//		event.preventDefault();
+//		
+//		var id = $(this).attr('href');
+//		
+//		var top = $(id).offset().top;
+//		$('body, html').animate({scrollTop: top}, 300);
+//	});
+//	
+//	
+//	
+//
+//	
+//	
+//});
+
+
+
+
+
+$(".main").onepage_scroll();
+
+
+
+//$(".onepage-pagination ").add("<p>");
+//for (i = 0; i < 4; i++) {
+//	$(".onepage-pagination li a:nth-child(i)").append(navLinks[i]);
+//}
+//$(".onepage-pagination li a").append(" class='left-nav'>О чем пойдет речь? <br> Что я получу. <br> Отзывы <br> забрать");
+//$(".onepage-pagination li a").css({"content": " &nbsp"});
+//$(".onepage-pagination ").css({"float": "left"});
+//$(".left-nav").css({"margin" : "-45px 1px 2px 30px", "line-height": "1.5"});
